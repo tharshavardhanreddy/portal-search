ebce882544786aa7b43acc668158ea70bf0e581a
tharshavardhanreddy <48549064+tharshavardhanreddy@users.noreply.github.com>
2019-08-12 19:05:54 +0530

    'styling'

--- src/app/data-table/data-table-datasource.ts
+++ src/app/data-table/data-table-datasource.ts
@@ -8,30 +8,31 @@ import { Observable, of as observableOf, merge } from 'rxjs';
 export interface DataTableItem {
   name: string;
   id: number;
+  exp: number;
 }
 
 // TODO: replace this with real data from your application
 const EXAMPLE_DATA: DataTableItem[] = [
-  {id: 1, name: 'Hydrogen'},
-  {id: 2, name: 'Helium'},
-  {id: 3, name: 'Lithium'},
-  {id: 4, name: 'Beryllium'},
-  {id: 5, name: 'Boron'},
-  {id: 6, name: 'Carbon'},
-  {id: 7, name: 'Nitrogen'},
-  {id: 8, name: 'Oxygen'},
-  {id: 9, name: 'Fluorine'},
-  {id: 10, name: 'Neon'},
-  {id: 11, name: 'Sodium'},
-  {id: 12, name: 'Magnesium'},
-  {id: 13, name: 'Aluminum'},
-  {id: 14, name: 'Silicon'},
-  {id: 15, name: 'Phosphorus'},
-  {id: 16, name: 'Sulfur'},
-  {id: 17, name: 'Chlorine'},
-  {id: 18, name: 'Argon'},
-  {id: 19, name: 'Potassium'},
-  {id: 20, name: 'Calcium'},
+  {id: 1, name: 'Hydrogen', exp: 2},
+  {id: 2, name: 'Helium', exp: 2},
+  {id: 3, name: 'Lithium', exp: 2},
+  {id: 4, name: 'Beryllium', exp: 2},
+  {id: 5, name: 'Boron', exp: 2},
+  {id: 6, name: 'Carbon', exp: 2},
+  {id: 7, name: 'Nitrogen', exp: 2},
+  {id: 8, name: 'Oxygen', exp: 2},
+  {id: 9, name: 'Fluorine', exp: 2},
+  {id: 10, name: 'Neon', exp: 2},
+  {id: 11, name: 'Sodium', exp: 2},
+  {id: 12, name: 'Magnesium', exp: 2},
+  {id: 13, name: 'Aluminum', exp: 2},
+  {id: 14, name: 'Silicon', exp: 2},
+  {id: 15, name: 'Phosphorus', exp: 2},
+  {id: 16, name: 'Sulfur', exp: 2},
+  {id: 17, name: 'Chlorine', exp: 2},
+  {id: 18, name: 'Argon', exp: 2},
+  {id: 19, name: 'Potassium', exp: 2},
+  {id: 20, name: 'Calcium', exp: 2},
 ];
 
 /**
--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -8,30 +8,31 @@ import {MatTableDataSource} from '@angular/material';
 export interface DataTableItem {
   name: string;
   id: number;
+  exp: number;
 }
 
 
 const EXAMPLE_DATA: DataTableItem[] = [
-  {id: 1, name: 'Hydrogen'},
-  {id: 2, name: 'Helium'},
-  {id: 3, name: 'Lithium'},
-  {id: 4, name: 'Beryllium'},
-  {id: 5, name: 'Boron'},
-  {id: 6, name: 'Carbon'},
-  {id: 7, name: 'Nitrogen'},
-  {id: 8, name: 'Oxygen'},
-  {id: 9, name: 'Fluorine'},
-  {id: 10, name: 'Neon'},
-  {id: 11, name: 'Sodium'},
-  {id: 12, name: 'Magnesium'},
-  {id: 13, name: 'Aluminum'},
-  {id: 14, name: 'Silicon'},
-  {id: 15, name: 'Phosphorus'},
-  {id: 16, name: 'Sulfur'},
-  {id: 17, name: 'Chlorine'},
-  {id: 18, name: 'Argon'},
-  {id: 19, name: 'Potassium'},
-  {id: 20, name: 'Calcium'},
+  {id: 1, name: 'Hydrogen', exp: 2},
+  {id: 2, name: 'Helium', exp: 2},
+  {id: 3, name: 'Lithium', exp: 2},
+  {id: 4, name: 'Beryllium', exp: 2},
+  {id: 5, name: 'Boron', exp: 2},
+  {id: 6, name: 'Carbon', exp: 2},
+  {id: 7, name: 'Nitrogen', exp: 2},
+  {id: 8, name: 'Oxygen', exp: 2},
+  {id: 9, name: 'Fluorine', exp: 2},
+  {id: 10, name: 'Neon', exp: 2},
+  {id: 11, name: 'Sodium', exp: 2},
+  {id: 12, name: 'Magnesium', exp: 2},
+  {id: 13, name: 'Aluminum', exp: 2},
+  {id: 14, name: 'Silicon', exp: 2},
+  {id: 15, name: 'Phosphorus', exp: 2},
+  {id: 16, name: 'Sulfur', exp: 2},
+  {id: 17, name: 'Chlorine', exp: 2},
+  {id: 18, name: 'Argon', exp: 2},
+  {id: 19, name: 'Potassium', exp: 2},
+  {id: 20, name: 'Calcium', exp: 2},
 ];
 
 
@@ -50,7 +51,7 @@ export class DataTableComponent implements AfterViewInit, OnInit {
   dataSource = new MatTableDataSource(EXAMPLE_DATA);
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ['id', 'name', 'exp'];
 
   ngOnInit() {
     // this.dataSource = new DataTableDataSource();
